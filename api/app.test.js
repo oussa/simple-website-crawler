9ed7e232802dc04c8cc94bbf3234b1de1bf92ea0
Oussama Krifa <krifa.oussama@gmail.com>
2017-10-20 03:23:46 +0200

    re-enable all api tests, fix 1 broken test, bump version number

--- api/app.test.js
+++ api/app.test.js
@@ -6,23 +6,23 @@ const { validUrl, invalidUrl, validUrls, validUrlAnalysis, validUrlHTML} = requi
 jest.mock('./fetchUtils')
 
 describe('Basic checks', () => {
-  xtest('API is up and running', () => {
+  test('API is up and running', () => {
     return request(app).get('/health').expect(200)
   })
-  xtest('API gives usage examples', async () => {
+  test('API gives usage examples', async () => {
     const response = await request(app).get('/')
     expect(response.text).toMatch(/Usage/)
   })
 })
 
 describe('API works as expected', () => {
-  xtest('Informs about missing params', async () => {
-    const response = await request(app).get('/crawl')
+  test('Informs about missing params', async () => {
+    const response = await request(app).get('/crawl/')
     expect(response.statusCode).toBe(400);
     expect(response.text).toMatch(/Bad request/);
   });
 
-  xtest('Returns website summary', async () => {
+  test('Returns website summary', async () => {
     const response = await request(app).get(`/crawl/${validUrl}`)
     expect(response.statusCode).toBe(200);
     expect(response.body).toEqual(validUrlAnalysis);
@@ -33,7 +33,7 @@ describe('api utils functions work', () => {
   test('fetchWebsite returns html with valid url', async () => {
     await expect(fetchWebsite(validUrl)).resolves.toEqual(validUrls[validUrl]);
   })
-  xtest('fetchWebsite returns error with invalid url', async () => {
+  test('fetchWebsite returns error with invalid url', async () => {
     await expect(fetchWebsite(invalidUrl)).rejects.toEqual({
       error: `website at url ${invalidUrl} could not be found`,
     });
